a08a12385cc4e118757281de499189f1aa30b5bd
Jayant Arora <jaistudy1996@gmail.com>
2018-10-10 08:36:07 -0400

    Add timer which will quit the application after timer has expired.

--- gophercises/quiz/quiz.go
+++ gophercises/quiz/quiz.go
@@ -8,20 +8,28 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	quiz := readCSV("./problems.csv")
 	correctAnswers := 0
+	answeredQues := 0
+	timer := time.AfterFunc(time.Second*30, func() {
+		fmt.Printf("\nScore: %d/%d. Total questions answered: %d",
+			correctAnswers, len(quiz), answeredQues)
+		os.Exit(0) // Exit when we are out of time.
+	})
+	defer timer.Stop()
 	for _, ques := range quiz {
 		consoleReader := bufio.NewReader(os.Stdin)
 		fmt.Print(ques[0], " ")
 		answer, _ := consoleReader.ReadString('\n')
+		answeredQues++
 		if strings.TrimRight(answer, "\n") == ques[1] {
 			correctAnswers++
 		}
 	}
-	fmt.Printf("Score: %d/%d", correctAnswers, len(quiz))
 }
 
 func checkError(e error) {
